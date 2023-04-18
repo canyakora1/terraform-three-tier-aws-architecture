6578e2bf809508b564d4747ff6b482134890a89b
canyakora1 <anyakorachinedu@yahoo.com>
2023-04-17 22:04:52 -0400

    initial design commit

---
+++ provider.tf
@@ -0,0 +1,18 @@
+terraform {
+  required_version = ">= 1.2.0"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.61.0"
+    }
+  }
+
+}
+
+provider "aws" {
+  region                   = "us-east-1"
+  shared_config_files      = ["~/.aws/conf"]
+  shared_credentials_files = ["~/.aws/credentials"]
+  profile                  = "vscode"
+}
